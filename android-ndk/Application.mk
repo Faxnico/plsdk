2480a57ad4f52e52a67b9aaef3d6ceab7874d754
Guillaume Tucker <guillaume.tucker@plasticlogic.com>
2013-04-23 16:38:03 +0100

    android-ndk/Application.mk: add plottest

--- android-ndk/Application.mk
+++ android-ndk/Application.mk
@@ -2,6 +2,6 @@
 PLASTICLOGIC_NDK_BUILD := 1
 APP_PLATFORM := android-8
 APP_MODULES := \
-	epview eptest plinput \
+	epview eptest plinput plottest \
 	mod_plepaper_imx mod_plepaper_plepdc mod_plepaper_epson \
 	_plsdk _imaging
